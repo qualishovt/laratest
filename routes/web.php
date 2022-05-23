0b711c9cc2526856227b24b05f7070fbcc9a2f34
Tehran <tehran_alishov@hotmail.com>
2022-05-23 12:43:03 +0300

    redirect to posts

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return redirect('/posts');
 });
 
 Route::get('posts', [PostController::class, 'index']);
