68d087db3d514e99f29cb70c193e318e0a9f29f4
Tehran <tehran_alishov@hotmail.com>
2022-05-23 12:40:29 +0300

    pagination per 10

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,7 +14,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::orderBy('id')->paginate(5);
+        $posts = Post::orderBy('id')->paginate(10);
         return view('posts.index', compact('posts'));
     }
 
